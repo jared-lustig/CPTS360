f4e1245eb581a387d48f22a86f8214534c493d13
lustig <jared.lustig@wsu.edu>
2022-03-23 18:22:40 -0700

    lab6 done

--- cd_ls_pwd.c
+++ cd_ls_pwd.c
@@ -18,15 +18,15 @@ int cd()
   }
 }
 
-int ls_file(MINODE *mip, char *name)
-{
-  printf("ls_file: to be done: READ textbook!!!!\n");
-  // READ Chapter 11.7.3 HOW TO ls
-}
+// int ls_file(MINODE *mip, char *name)
+// {
+//   printf("ls_file: to be done: READ textbook!!!!\n");
+//   // READ Chapter 11.7.3 HOW TO ls
+// }
 
 int ls_dir(MINODE *mip)
 {
-  printf("ls_dir: list CWD's file names; YOU FINISH IT as ls -l\n");
+  //printf("ls_dir: list CWD's file names; YOU FINISH IT as ls -l\n");
 
   char buf[BLKSIZE], temp[256];
   DIR *dp;
@@ -35,21 +35,46 @@ int ls_dir(MINODE *mip)
   get_block(dev, mip->INODE.i_block[0], buf);
   dp = (DIR *)buf;
   cp = buf;
-  
+
   while (cp < buf + BLKSIZE){
-     int ino = getino(pathname);
-     mip = iget(dev, ino);
+     //int ino = getino(pathname);
+     mip = iget(dev, dp->inode);
+     
+      char *t1 = "xwrxwrxwr-------";
+      char *t2 = "----------------";
+
+      if ((mip->INODE.i_mode & 0xF000) == 0x8000) // if (S_ISREG())
+      printf("%c",'-');
+      if ((mip->INODE.i_mode & 0xF000) == 0x4000) // if (S_ISDIR())
+      printf("%c",'d');
+      if ((mip->INODE.i_mode & 0xF000) == 0xA000) // if (S_ISLNK())
+      printf("%c",'l');
+      for (int i=8; i >= 0; i--){
+        if (mip->INODE.i_mode & (1 << i)) // print r|w|x
+          printf("%c", t1[i]);
+        else
+          printf("%c", t2[i]); // or print -
+      }
 
      strncpy(temp, dp->name, dp->name_len);
      temp[dp->name_len] = 0;
 	
-     ls_file(mip, temp);
-     iput(mip);
+    //  ls_file(mip, temp);
+    //  iput(mip);
 
-     printf("%s  ", temp);
+     int gid, uid;
+     gid = getgid(); uid = geteuid();
+     char mtime[64];
+     ctime_r((time_t *)&mip->INODE.i_mtime, mtime);
+     mtime[strlen(mtime) - 1] = 0;
+
+     printf("%8u %8u %8u %s %8u %s \t[%d %2d]\n", mip->INODE.i_links_count, 
+     mip->INODE.i_gid, mip->INODE.i_uid, mtime, mip->INODE.i_size, temp, mip->dev, mip->ino);
 
      cp += dp->rec_len;
      dp = (DIR *)cp;
+
+     
   }
   printf("\n");
 }
